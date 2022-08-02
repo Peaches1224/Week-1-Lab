85423192c606b2d2941e40c37c3566db03a3b10a
Chawntel Stitts <luckypeach45@gmail.com>
2022-08-02 16:09:00 -0700

    updatedcode

---
+++ scripts.js
@@ -0,0 +1,44 @@
+//create a variable to hold the quantity of available plane seats left on a flight
+let availablePlaneSeats = 6; 
+		
+//create a variable to hold the cost of groceries at checkout
+let costOfGroceries = 23.64;
+
+//create a variable to hold a person's middle initial
+let middleInitial = "N";
+
+//create a variable to hold true if it's hot outside and false if it's cold outside
+let isHotOutside = true;
+//create a variable to hold a customer's first name
+let customerFirstName = "Sally";
+
+//create a variable to hold a street address
+let streetAdress = "1234 W Easy St";
+
+//print all variables to the console
+console.log(availablePlaneSeats);
+console.log(costOfGroceries);
+console.log(middleInitial);
+console.log(isHotOutside);
+console.log(customerFirstName);
+console.log(streetAdress);
+
+
+
+//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+availablePlaneSeats = availablePlaneSeats - 2;
+console.log(availablePlaneSeats);
+//impulse candy bar purchase, add 2.15 to the grocery total
+//costOfGroceries = costOfGroceries + 2.15;
+costOfGroceries = costOfGroceries + 2.15
+console.log(costOfGroceries);
+//birth certificate was printed incorrectly, change the middle initial to something else
+middleInitial = "C"
+console.log(middleInitial);
+//the season has changed, update the hot outside variable to be opposite of what it was
+
+
+//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+
+
+//print a line to the console that introduces the customer and says they live at the address variable
